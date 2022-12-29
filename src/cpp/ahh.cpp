c4c8b09037983e15e186b43b19d1cc634fa1890f
werdl <phpchat@outlook.com>
2022-12-29 18:25:18 +0000

    first lexing done

--- src/cpp/ahh.cpp
+++ src/cpp/ahh.cpp
@@ -4,18 +4,33 @@ using namespace std;
 
 int main()
 {
-	string given_str = "hello;world";
-	string token0;
-	string token1;
-	string delim=";";
-	size_t pos = 0;
-	string tokens[4];
-	while (( pos = given_str.find (delim)) != std::string::npos)
-	{
-	token1 = given_str.substr(0, pos); // store the substring 
-	tokens[0]=token1;
-	given_str.erase(0, pos + delim.length());  /* erase() function store the current positon and move to next token. */ 
+	bool shell=true;
+	while (shell=true) {
+		//start getting string and splitting
+		string given_str;
+		cout << ">";
+		cin >> given_str;
+		string token0;
+		string token1;
+		string delim="~";
+		size_t pos = 0;
+		string tokens[4];
+		while (( pos = given_str.find (delim)) != std::string::npos)
+		{
+		token1 = given_str.substr(0, pos); // store the substring 
+		tokens[0]=token1;
+		given_str.erase(0, pos + delim.length());  /* erase() function store the current positon and move to next token. */ 
+		}
+		tokens[1]=given_str;
+		//end getting string
+		string print="print";
+		string exit="exit";
+		if (tokens[0]==print) {
+			cout <<tokens[1]<<endl;
+		}
+		if (tokens[0]==exit) {
+			shell=false;
+		}
+		cout << "\n";
 	}
-tokens[1]=given_str;
-cout << tokens[0] << endl << tokens[1] << endl;
 }
