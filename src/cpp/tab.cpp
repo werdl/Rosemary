9a5b482b029e84c6c9d620276887adda61e01ba0
werdl <phpchat@outlook.com>
2022-12-30 09:58:12 +0000

    some progress

--- src/cpp/tab.cpp
+++ src/cpp/tab.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <vector>
 #include <algorithm>
+#include <map>
 
 using std::cout; using std::cin;
 using std::endl; using std::string;
@@ -32,11 +33,34 @@ int main(){
     }
 	string print="print";
 	string exita="exit";
+    string dollar = "int";
+    string bob = "view";
+    char dollr = '$';
+    string tokens0=tokens[0];
+    std::map<string, string>  variables;
+    
+    string tokens0dupe=tokens[0];
+
 		if (tokens[0]==print) {
 			cout <<tokens[1]<<endl;
 		}
 		if (tokens[0]==exita) {
 			exit(0);
 		}
+        if (tokens[0]==dollar) {string dollar = "int";
+            variables[tokens[1]]=tokens[3];
+            for (auto& x: variables) {
+                
+            std::cout <<  x.second << '\n';
+                
+  }
+            cout << "assign";
+        }
+        if (tokens[0]==bob) {
+            tokens0dupe.erase(0, 1);
+            cout << "get";
+            cout << tokens[1];
+            
+        }
     }
 }
