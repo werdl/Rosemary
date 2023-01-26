906041538c5b7fb81060483fa3b2182b3e9eb4b2
werdl <116349156+werdl@users.noreply.github.com>
2023-01-26 07:51:47 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,6 @@
-set /P INPUT=Type input: %=%
+set /p INPUT= Install Directory:
+IF %INPUT%==null (echo Installing In Home Directory) 
+IF %INPUT%==null (%INPUT%=HOME)
 
 curl https://github.com/TheRosemaryProject/Rosemary/tree/main/src/cpp/rsmy.exe > %INPUT$/rsmy.exe
 curl https://github.com/TheRosemaryProject/Sprig/blob/main/sprig.exe > %INPUT$/sprig.exe
