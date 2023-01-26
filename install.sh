db99284325ddb3cb7f75317d2bb61e2ed58a754a
werdl <116349156+werdl@users.noreply.github.com>
2023-01-26 07:55:44 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,4 @@
-
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+   curl https://github.com/TheRosemaryProject/Rosemary/tree/main/src/cpp/rsmy.unix > $INPUT/rsmy.unix
+   curl https://github.com/TheRosemaryProject/Sprig/blob/main/sprig.unix > $INPUT/sprig.unix
+   curl https://github.com/TheRosemaryProject/Sprig/blob/main/json.hpp > $INPUT/json.hpp
