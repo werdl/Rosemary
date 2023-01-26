3bd2cccafa26169828f9ea251e7cd1146f2eac0a
werdl <116349156+werdl@users.noreply.github.com>
2023-01-26 07:56:17 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,9 +2,9 @@ set /p INPUT= Install Directory:
 IF %INPUT%==null (echo Installing In Home Directory) 
 IF %INPUT%==null (%INPUT%=HOME)
 
-curl https://github.com/TheRosemaryProject/Rosemary/tree/main/src/cpp/rsmy.exe > %INPUT$/rsmy.exe
-curl https://github.com/TheRosemaryProject/Sprig/blob/main/sprig.exe > %INPUT$/sprig.exe
-curl https://github.com/TheRosemaryProject/Sprig/blob/main/json.hpp > %INPUT$/json.hpp
+curl https://github.com/TheRosemaryProject/Rosemary/tree/main/src/cpp/rsmy.exe > %INPUT%/rsmy.exe
+curl https://github.com/TheRosemaryProject/Sprig/blob/main/sprig.exe > %INPUT%/sprig.exe
+curl https://github.com/TheRosemaryProject/Sprig/blob/main/json.hpp > %INPUT%/json.hpp
 
 setx path "%path%;%INPUT%"
 rsmy
