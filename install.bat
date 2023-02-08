327871667703d69b4c520acfb993452428d120b6
werdl <116349156+werdl@users.noreply.github.com>
2023-02-08 17:55:57 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 @ECHO off
+title Installing Rosemary
 set /p INPUT= Install Directory: 
 IF %INPUT%==null (echo Installing In %cd%) 
 IF %INPUT%==null (mkdir rsmy)
@@ -7,4 +8,34 @@ setx rsmypath %INPUT%
 curl https://raw.githubusercontent.com/TheRosemaryProject/Rosemary/main/src/cpp/rsmy.exe > %INPUT%+'/rsmy.exe'
 curl https://raw.githubusercontent.com/TheRosemaryProject/Sprig/main/sprig.exe > %INPUT%+'/sprig.exe'
 setx path "%path%;%INPUT%"
+echo [~---------]
+ping -n 2 localhost >nul
+cls
+echo [~~--------]
+ping -n 2 localhost  >nul
+cls
+echo [~~~-------]
+ping -n 2 localhost  >nul
+cls
+echo [~~~~------]
+ping -n 2 localhost  >nul
+cls
+echo [~~~~~-----]
+ping -n 2 localhost  >nul
+cls
+echo [~~~~~~----]
+ping -n 2 localhost  >nul
+cls
+echo [~~~~~~~---]
+ping -n 2 localhost  >nul
+cls
+echo [~~~~~~~~--]
+ping -n 2 localhost  >nul
+cls
+echo [~~~~~~~~~-]
+ping -n 2 localhost >nul
+cls
+echo [~~~~~~~~~~]
+echo Installed!
+pause
 rsmy
