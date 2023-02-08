53189a172ab1264114372d1057f7d4577976623f
werdl <116349156+werdl@users.noreply.github.com>
2023-02-08 07:45:28 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,12 +1,11 @@
-%ECHO% off
+@ECHO off
 set /p INPUT= Install Directory: 
-IF %INPUT%==null (echo Installing In Home Directory) 
+IF %INPUT%==null (echo Installing In %cd%) 
 IF %INPUT%==null (mkdir rsmy)
 IF %INPUT%==null (%INPUT%=%UserProfile%+'rsmy')
-
+setx rsmypath %INPUT%
 curl https://raw.githubusercontent.com/TheRosemaryProject/Rosemary/main/src/cpp/rsmy.exe > %INPUT%+'/rsmy.exe'
 curl https://raw.githubusercontent.com/TheRosemaryProject/Sprig/main/sprig.exe > %INPUT%+'/sprig.exe'
 curl https://raw.githubusercontent.com/TheRosemaryProject/Sprig/main/json.hpp > %INPUT%+'/json.hpp'
-
 setx path "%path%;%INPUT%"
 rsmy
