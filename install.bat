0ed62f846ca57170e23a6ec609f0987347d82bd6
werdl <116349156+werdl@users.noreply.github.com>
2023-02-09 17:55:48 +0000

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,8 @@
 title Installing Rosemary
 echo Installing in %cd%/rsmy
 setx rsmypath %cd%/rsmy
-setx path "%path%;%INPUT%"cd%/rsmy
+setx path "%path%;%cd%/rsmy"
+cd %cd%/rsmy
 echo [~---------]
 ping -n 2 localhost >nul
 cls
